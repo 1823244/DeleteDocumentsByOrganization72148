6942e56ae3f3a6069755e531a034bd46b9b65231
nikolai <1823244@mail.ru>
2018-05-26 20:40:30 +0300

    добавлен скрипт для распаковки без помещения в git

---
+++ unpack.cmd
@@ -0,0 +1,3 @@
+chcp 1251
+echo "unpack erf and epf"
+oscript -encoding=utf-8 .git\hooks\v8files-extractor.os --decompile УдалениеДокументовПоОрганизации_УТ_БП_82_72148_УФ.epf src\УдалениеДокументовПоОрганизации_УТ_БП_82_72148_УФ
